2cca2198f3d54fd815ddf7723601708d15838819
Jaise152 <53616607+Jaise152@users.noreply.github.com>
2019-09-05 17:27:39 +0530

    Update Karatsuba2.py

--- Karatsuba2.py
+++ Karatsuba2.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep  5 11:44:05 2019
+Created on Thu Sep  5 08:44:08 2019
 
 @author: 611916967
 """
-
 def ceil(n):
     res = int(n)
     return res if res == n or n < 0 else res+1
@@ -13,8 +12,7 @@ def floor(n):
     res = int(n)
     return res if res == n or n >= 0 else res-1
 
-    
-def subquadratic(x,y):
+def karastuba(x,y):
     if x < 10 and y <10:
         return x*y
     n = max(len(str(x)),len(str(y)))
@@ -29,23 +27,21 @@ def subquadratic(x,y):
     openfile.writelines("A : "+ str(x) + " A1 : "+ str(x_H) +" A2 : "+ str(x_L)+ "\n")
     openfile.writelines("B : "+ str(y) + " B1 : "+ str(y_H) +" B2 : "+ str(y_L)+ "\n")
     openfile.close()
-    S1 = subquadratic(x_H,y_H)
-    S2 = subquadratic(x_L,y_L)
-    S3 = subquadratic(x_H,y_L)
-    S4 = subquadratic(x_L,y_H)
-    S5 = subquadratic(x_H + x_L , y_H + y_L)
-    S6 = S5 - S1 -S2
-    Karatsuba_with_complexity = int(S1*(10**(m*2))+(S3+S4)*(10**(m))+S2) #time complexity n^2
-    Karatsuba = int(S1*(10**(m*2))+S6*(10**m)+S2)
-    return Karatsuba
+#    print(y_H,y_L)
+    S1 = karastuba(x_H,y_H)
+    S2 = karastuba(x_L,y_L)
+    S3 = karastuba(x_H + x_L , y_H + y_L)    
+    S4 = S3 - S1 -S2
+    S5 = int(S1*(10**(m*2))+S4*(10**(m))+S2)
+    return S5
+
 def readfile(filename):
     f = open(filename, "r")
     f1 = f.readlines()
     x = int(f1[0])
     y = int(f1[1])
     return(x,y)
-       
-    
+
 x = readfile('InputPS2.txt')[0]
 y = readfile('InputPs2.txt')[1]
 openfile = open("outputPS2.txt","a")
@@ -57,5 +53,5 @@ openfile.close()
 #print(y)
 openfile = open("outputPS2.txt","a")
 openfile.writelines("-----------------------------------------" + "\n")
-openfile.writelines(" Result:> "+str(x)+" * "+str(y)+" = "+ str(subquadratic(x,y)))
+openfile.writelines(" Result:> "+str(x)+" * "+str(y)+" = "+ str(karastuba(x,y)))
 openfile.close()
